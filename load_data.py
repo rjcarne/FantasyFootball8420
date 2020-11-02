0fd83028f96ff0a7efbee63950160c8198f86d64
Ryan Carney <rjcarne@g.clemson.edu>
2020-11-01 20:23:40 -0500

    fixed labels

--- load_data.py
+++ load_data.py
@@ -22,16 +22,16 @@ def append_data(X, filepath, year):
         key_col[i] = data_array[i][0] + str(year)
     key_col[0] = "Key"
     data_array = np.hstack((data_array[:,:0], key_col, data_array[:,0:]))
-    # print(key_col.shape)
-    # print(data_array.shape)
+  
+
     if year <= 1991:
         PLAYER_NUM = data_array.shape[0]
         new_col = np.empty((PLAYER_NUM,1))
         for current_player in range(1,PLAYER_NUM):
             new_col[current_player] = data_array[current_player][12]
         data_array = np.hstack((data_array[:,:12], new_col, data_array[:,12:]))
-        data_array = np.delete(data_array, 0, axis=0)
-        
+    data_array = np.delete(data_array, 0, axis=0)
+    
     X = np.append(X, values=data_array,axis=0)
     return X
 
@@ -42,14 +42,27 @@ yearly_directory = 'Fantasy_Data/yearly/'
 for year in range(1970,2020):
     filepath = yearly_directory + str(year) + '.csv'
     X = append_data(X, filepath, year)
-    X_labels = X[0]
+    
+with open(filepath,'r') as dest_f:
+        data_iter = csv.reader(dest_f,
+                            delimiter = ',',
+                            quotechar = '"')
+        data = [data for data in data_iter]
+data_array = np.asarray(data, dtype = None)
+data_array = np.delete(data_array,0,1)
+
+PLAYER_NUM = data_array.shape[0]
+
+key_col = np.empty((PLAYER_NUM,1))
+key_col = key_col.astype('str') 
+for i in range(1,PLAYER_NUM):
+    key_col[i] = data_array[i][0] + str(year)
+key_col[0] = "Key"
+data_array = np.hstack((data_array[:,:0], key_col, data_array[:,0:]))
+
+X_labels = data_array[0]
 
 
-# with open(yearly_directory + '2019.csv','r') as dest_f:
-#         data_iter = csv.reader(dest_f,
-#                             delimiter = ',',
-#                             quotechar = '"')
-#         data = [data for data in data_iter]
-#         data_array = np.asarray(data, dtype = None)
-#         X_labels = data_array[0]
+    
+
 
