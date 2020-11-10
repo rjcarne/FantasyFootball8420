e2a1916bc449d809992eca62a804444e35897841
blakewashburn <blakewashburn1@gmail.com>
2020-11-09 22:10:15 -0500

    My first commit

--- regression.py
+++ regression.py
@@ -105,14 +105,61 @@ def get_polynomial_regression(X, Variable, Position):
 def get_average(X):
     pass
 
-X = get_all_data()
 
-Y = get_player_df(X, "Tom Brady")
+def main():    
+    mydf = get_all_data()
+    
+    # Tom Brady Data
+    playerdf = get_player_df(mydf, 'Tom Brady')
+    bradyPoints = []
+    bradyYears = []
+    for value in playerdf['FantasyPoints']:
+        bradyPoints.append(value)
+    for value in playerdf['Year']:
+        bradyYears.append(value)  
+        
+        
+    # Peyton Manning Data
+    playerdf = get_player_df(mydf, 'Peyton Manning')
+    manningPoints = []
+    manningYears = []
+    for value in playerdf['FantasyPoints']:
+        manningPoints.append(value)
+    for value in playerdf['Year']:
+        manningYears.append(value)
+        
+    # Matt Cassel
+    playerdf = get_player_df(mydf, 'Matt Cassel')
+    casselPoints = []
+    casselYears = []
+    for value in playerdf['FantasyPoints']:
+        casselPoints.append(value)
+    for value in playerdf['Year']:
+        casselYears.append(value)
+    
+    # Visualizations
+    plt.plot(bradyYears, bradyPoints, 'o', color='red')
+    plt.plot(manningYears, manningPoints, 'x', color='blue')
+    plt.plot(casselYears, casselPoints, '+', color='green')
+    plt.title("QB Fantasy Points by Year")
+    plt.xlabel("Years") 
+    plt.ylabel("Points")
+    plt.show()
+    
+
+
+main()
+
+
+
+# X = get_all_data()
+
+# Y = get_player_df(X, "Tom Brady")
 # Y = X[X["Age"] >= 28]
 
 
 # Y = X[X["Player"] == 'Deshaun Watson']
-get_ridge_regression(Y, "PassingYds", "QB")
+# get_ridge_regression(Y, "PassingYds", "QB")
 # get_linear_regression(Y)
 # coeff = get_ridge_regression(Y)
 # plt.plot(coeff)
